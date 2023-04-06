4b35a11ac09498d3c260d7036fde34a936f3b270
HyukHyunCho <gurgus0189@gmail.com>
2023-04-06 23:15:16 +0900

    Feat: 게시물 리스트 및 페이지네이션

--- src/containers/Home/HomeForm.tsx
+++ src/containers/Home/HomeForm.tsx
@@ -1,38 +1,22 @@
-import React from 'react';
-import { Container, CssBaseline, Box, Typography } from '@mui/material';
 import { useProduct } from '../../hooks/useProducts';
 import ProductList from '../../components/product/productList';
 import { Pagination } from '@mui/material';
+import Layout from '../../components/common/Layout';
 
 export default function HomeForm() {
   const { products, maxPage, setCurrentPage } = useProduct();
 
   return (
-    <Container component="main" maxWidth="md">
-      <CssBaseline />
-      <Box
-        sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
+    <Layout title={'상품 리스트'} size={1000}>
+      {products && <ProductList products={products} />}
+      <Pagination
+        count={maxPage}
+        color="primary"
+        onChange={(_, page) => {
+          setCurrentPage(page);
+          window.scrollTo(0, 0);
         }}
-      >
-        <Typography component="h1" variant="h5">
-          상품 리스트
-        </Typography>
-        <Box sx={{ mt: 1 }}>
-          {products && <ProductList products={products} />}
-        </Box>
-        <Pagination
-          count={maxPage}
-          color="primary"
-          onChange={(_, page) => {
-            setCurrentPage(page);
-            window.scrollTo(0, 0);
-          }}
-        />
-      </Box>
-    </Container>
+      />
+    </Layout>
   );
 }
