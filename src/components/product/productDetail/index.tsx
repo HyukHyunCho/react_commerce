63cb04db4c387e7bb6b6f54fba9d585c8b19acb9
HyukHyunCho <gurgus0189@gmail.com>
2023-04-06 23:18:34 +0900

    Design: 상품상세 UI 컴포넌트 추가

--- src/components/product/productDetail/index.tsx
+++ src/components/product/productDetail/index.tsx
@@ -36,67 +36,59 @@ interface IProductProps {
 
 export default function ProductDetail({
   productDetailData,
-  cartData,
   count,
   onCountClick,
   onClickAddCart,
 }: IProductProps) {
   console.log(productDetailData);
   return (
-    <Container component="main" maxWidth="lg">
-      <CssBaseline />
-      <Box sx={{ width: '100%', marginTop: 8 }}>
-        {productDetailData && (
-          <Grid
-            container
-            rowSpacing={1}
-            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-          >
-            <Grid xs={6}>
-              <img
-                className={style.img}
-                src={productDetailData.image}
-                alt={productDetailData.image}
-              />
-            </Grid>
-            <Grid xs={6}>
-              <Typography>{productDetailData.store_name}</Typography>
-              <div>{productDetailData.product_name}</div>
-              <div>
-                {productDetailData.shipping_method === 'PARCEL'
-                  ? '택배배송'
-                  : '직접배송(화물배달)'}
-              </div>
-              <div>재고 : {productDetailData.stock}</div>
-              <div className={style.countContainer}>
-                <button
-                  className={style.countBtn}
-                  onClick={() => onCountClick('minus')}
-                >
-                  -
-                </button>
-                <div className={style.count}>{count}</div>
-                <button
-                  className={style.countBtn}
-                  onClick={() => onCountClick('plus')}
-                >
-                  +
-                </button>
-              </div>
-              <div>총 상품 금액 : </div>
-              <Button variant="contained">바로구매</Button>
-              <Button
-                type="button"
-                variant="contained"
-                color="secondary"
-                onClick={() => onClickAddCart()}
-              >
-                장바구니
-              </Button>
-            </Grid>
+    <>
+      {productDetailData && (
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <Grid xs={6}>
+            <img
+              className={style.img}
+              src={productDetailData.image}
+              alt={productDetailData.image}
+            />
           </Grid>
-        )}
-      </Box>
-    </Container>
+          <Grid xs={6}>
+            <Typography>{productDetailData.store_name}</Typography>
+            <div>{productDetailData.product_name}</div>
+            <div>
+              {productDetailData.shipping_method === 'PARCEL'
+                ? '택배배송'
+                : '직접배송(화물배달)'}
+            </div>
+            <div>재고 : {productDetailData.stock}</div>
+            <div className={style.countContainer}>
+              <button
+                className={style.countBtn}
+                onClick={() => onCountClick('minus')}
+              >
+                -
+              </button>
+              <div className={style.count}>{count}</div>
+              <button
+                className={style.countBtn}
+                onClick={() => onCountClick('plus')}
+              >
+                +
+              </button>
+            </div>
+            <div>총 상품 금액 : </div>
+            <Button variant="contained">바로구매</Button>
+            <Button
+              type="button"
+              variant="contained"
+              color="secondary"
+              onClick={() => onClickAddCart()}
+            >
+              장바구니
+            </Button>
+          </Grid>
+        </Grid>
+      )}
+    </>
   );
 }
