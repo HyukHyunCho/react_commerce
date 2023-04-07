07059b1966667e55aef57980d817bbf543da6bf4
HyukHyunCho <gurgus0189@gmail.com>
2023-04-08 07:16:51 +0900

    Feat: 바로구매 기능 추가

--- src/containers/ProductDetail/ProductDetailForm.tsx
+++ src/containers/ProductDetail/ProductDetailForm.tsx
@@ -2,11 +2,12 @@ import React, { useState } from 'react';
 import { useNavigate, useParams } from 'react-router';
 import ProductDetail from '../../components/product/productDetail';
 import { useProductDetail } from '../../hooks/useProductDetail';
-import { useCart, useCreateCart } from '../../hooks/useCart';
-import { getCookie } from '../../util/cookie';
+import { useAddCart, useCart } from '../../hooks/useCart';
+import { getUserType } from '../../util/cookie';
 import Modal from '../../components/modal';
 import Layout from '../../components/common/Layout';
 import { useShoppingCartCheck } from '../../hooks/useCartCheck';
+import { useAddOrder } from '../../hooks/useOrder';
 
 interface ICartItem {
   cart_item_id: number;
@@ -19,16 +20,16 @@ interface ICartItem {
 export default function ProductDetailForm() {
   const navigate = useNavigate();
   const { id } = useParams();
-  const [userType] = useState(getCookie('user_type'));
+  const [userType] = useState(getUserType());
   const [open, setOpen] = useState<boolean>(false);
   const [modalTitle, setModalTitle] = useState('');
   const [modalSubTitle, setModalSubTitle] = useState('');
   const [count, setCount] = useState(1);
   const { data: productDetailData } = useProductDetail(Number(id));
-
   const { data: cartCheckData } = useShoppingCartCheck();
-
-  const { mutate } = useCreateCart();
+  const { data: cartData, cartQueries } = useCart();
+  const { mutate: addCart } = useAddCart();
+  const { mutate: addPayment } = useAddOrder();
 
   const handleClose = () => {
     setOpen(false);
@@ -44,9 +45,15 @@ export default function ProductDetailForm() {
     }
   };
 
-  const onClickAddCart = () => {
-    if (!userType) return alert('로그인이 필요한 서비스입니다.');
-    if (userType === 'SELLER') return alert('판매자는 구매할 수 없습니다.');
+  const cartCheck = () => {
+    if (!userType) {
+      alert('로그인이 필요한 서비스입니다.');
+      return false;
+    }
+    if (userType === 'SELLER') {
+      alert('판매자는 구매할 수 없습니다.');
+      return false;
+    }
 
     const shoppingCartData = cartCheckData.find(
       (item: ICartItem) => item.product_id === productDetailData.product_id
@@ -57,15 +64,40 @@ export default function ProductDetailForm() {
       setModalTitle('장바구니에 있는 제품입니다.');
       setModalSubTitle('장바구니로 이동하시겠습니까?');
       setOpen(true);
-      return;
+      return false;
     }
+    return true;
+  };
+
+  const onClickOrder = () => {
+    const check = cartCheck();
+
+    if (!check) return;
+
+    navigate('/order/', {
+      state: {
+        orderItems: [
+          { product_id: productDetailData.product_id, quantity: count },
+        ],
+        orderCheckItems: [productDetailData],
+        cartItemTotalPrice: productDetailData.price * count,
+        cartItemFee: productDetailData.shipping_fee,
+        orderType: 'direct_order',
+      },
+    });
+  };
+
+  const onClickAddCart = () => {
+    const check = cartCheck();
+
+    if (!check) return;
 
     const req = {
       product_id: productDetailData.product_id,
       quantity: count,
       check: true,
     };
-    mutate(req, {
+    addCart(req, {
       onSuccess: (res) => {
         setModalTitle('상품이 장바구니에 추가 되었습니다.');
         setModalSubTitle('장바구니로 이동하시겠습니까?');
@@ -82,9 +114,9 @@ export default function ProductDetailForm() {
     <Layout title="상품 상세" size={1000}>
       <ProductDetail
         productDetailData={productDetailData}
-        //cartData={cartData}
         count={count}
         onCountClick={onCountClick}
+        onClickOrder={onClickOrder}
         onClickAddCart={onClickAddCart}
       />
       {open && (
