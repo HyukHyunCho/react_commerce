15441b34fa5549f2db597b9588e29ec59fe10b2f
HyukHyunCho <gurgus0189@gmail.com>
2023-04-08 07:16:06 +0900

    Refactor: 데이터 fetching -> queryParam 수정

--- src/containers/Order/OrderForm.tsx
+++ src/containers/Order/OrderForm.tsx
@@ -1,13 +1,24 @@
 import { useLocation } from 'react-router';
 import OrderList from '../../components/order/OrderList';
 import Layout from '../../components/common/Layout';
-import { Button } from '@mui/material';
-import OrderInfo from '../../components/order/OrderInfo';
+import OrderInfoForm from './OrderInfoForm';
 
 export default function OrderForm() {
   const {
-    state: { orderItems, orderCheckItems },
+    state: {
+      orderItems,
+      orderCheckItems,
+      cartItemTotalPrice,
+      cartItemFee,
+      orderType,
+    },
   } = useLocation();
+
+  // console.log(orderItems);
+  // console.log(orderCheckItems);
+  // console.log(cartItemTotalPrice);
+  // console.log(cartItemFee);
+  // console.log(orderType);
 
   return (
     <Layout title="주문하기" size={1000}>
@@ -17,22 +28,15 @@ export default function OrderForm() {
             orderItems={orderItems}
             orderCheckItems={orderCheckItems}
           />
-          <OrderInfo
+          <OrderInfoForm
             orderItems={orderItems}
             orderCheckItems={orderCheckItems}
+            cartItemTotalPrice={cartItemTotalPrice}
+            cartItemFee={cartItemFee}
+            orderType={orderType}
           />
         </>
       )}
-
-      <Button
-        type="button"
-        variant="contained"
-        fullWidth
-        sx={{ mt: 5 }}
-        // onClick={onClickOrder}
-      >
-        주문하기
-      </Button>
     </Layout>
   );
 }
