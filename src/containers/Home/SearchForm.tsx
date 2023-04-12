b11e9f55bdcf43ec11df0e1df4f6f48cfb85082d
HyukHyunCho <gurgus0189@gmail.com>
2023-04-12 23:26:09 +0900

    comment: 콘솔 삭제

--- src/containers/Home/SearchForm.tsx
+++ src/containers/Home/SearchForm.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import Layout from '../../components/common/Layout';
 import Typography from '@mui/material/Typography';
 import useDebounce from '../../hooks/useDebounce';
